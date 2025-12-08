996ad77ed09c2a694e699b2c05e29eff59f7226c
oticstps <oticsjetson@gmail.com>
2025-12-08 10:19:02 +0700

    Update filter_data_raw.sql

--- OTICS Plant 2/database/filter_data_raw.sql
+++ OTICS Plant 2/database/filter_data_raw.sql
@@ -7,3 +7,10 @@ WHERE col2 <> 'Main' OR col2 IS NULL;
 UPDATE nama_tabel
 SET col5 = SUBSTRING_INDEX(col5, '*', 1)
 WHERE col5 LIKE '%*%';
+
+=================Fitler 3
+UPDATE nama_tabel
+SET 
+    col6 = NULL,
+    col7 = NULL,
+    col8 = NULL;
