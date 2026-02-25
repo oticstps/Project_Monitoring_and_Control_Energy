dfb5d898118b2dca275fed60d80d0a11d4d18bd4
oticstps <oticsjetson@gmail.com>
2026-02-25 18:09:53 +0700

    Create sample.js

---
+++ OTICS Plant 1/sample.js
@@ -0,0 +1,15 @@
+{
+    "status": "success",
+    "message": "Data retrieved successfully",
+    "data": [
+        {
+            "date_time": "2026-02-25 16:55:56",
+            "panel": "kub_c",
+            "current_avg": "24.704",
+            "current_a": "23.681",
+            "current_b": "26.386",
+            "current_c": "24.044",
+            "current_n": "0.000"
+        }
+    ]
+}
